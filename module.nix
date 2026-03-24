a61bf90df4d6e30ec9e8a09ca996a20c624a105f
Stephan Daaboul <39669593+Daaboulex@users.noreply.github.com>
2026-03-24 15:06:44 +0100

    style: format all nix files with nixfmt

--- module.nix
+++ module.nix
@@ -1,23 +1,30 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   cfg = config.mesa-git;
 
-  mesaPkg = if cfg.drivers == [] then
-    pkgs.mesa-git
-  else
-    pkgs.mkMesaGit { vendors = cfg.drivers; };
+  mesaPkg = if cfg.drivers == [ ] then pkgs.mesa-git else pkgs.mkMesaGit { vendors = cfg.drivers; };
 
-  mesaPkg32 = if cfg.drivers == [] then
-    pkgs.mesa-git-32
-  else
-    pkgs.mkMesaGit32 { vendors = cfg.drivers; };
-in {
+  mesaPkg32 =
+    if cfg.drivers == [ ] then pkgs.mesa-git-32 else pkgs.mkMesaGit32 { vendors = cfg.drivers; };
+in
+{
   options.mesa-git = {
     enable = lib.mkEnableOption "Use mesa-git (bleeding-edge) instead of nixpkgs mesa";
 
     drivers = lib.mkOption {
-      type = lib.types.listOf (lib.types.enum [ "amd" "intel" "nvidia" ]);
-      default = [];
+      type = lib.types.listOf (
+        lib.types.enum [
+          "amd"
+          "intel"
+          "nvidia"
+        ]
+      );
+      default = [ ];
       example = [ "amd" ];
       description = ''
         GPU vendors to compile drivers for. Only the selected vendor drivers
